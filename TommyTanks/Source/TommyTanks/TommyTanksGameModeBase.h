e6b9a636d3c9c301ccd55bf6c2813d86350fff04
Tom Blackburn <hello.tomblackburn@gmail.com>
2019-05-27 14:04:00 +0100

    BT0004 - Created basic UE4 Project

---
+++ TommyTanks/Source/TommyTanks/TommyTanksGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TommyTanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOMMYTANKS_API ATommyTanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
